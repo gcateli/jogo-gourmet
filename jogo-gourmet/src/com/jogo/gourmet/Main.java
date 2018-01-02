a4372fdd0dc39ca29c182d6410e4defa3b0fc297
= <=>
2018-01-02 20:10:19 -0200

    Tela inicial do jogo

---
+++ jogo-gourmet/src/com/jogo/gourmet/Main.java
@@ -0,0 +1,11 @@
+package com.jogo.gourmet;
+
+import javax.swing.JOptionPane;
+
+public class Main {
+
+	public static void main(String[] args) {
+		JOptionPane.showMessageDialog(null, "Pense num prato que gosta");
+	}
+
+}
