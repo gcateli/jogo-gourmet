9f6def771546bccaad9b8a19e905327bd6d18718
= <=>
2018-01-03 21:41:47 -0200

    Adição de testes unitarios

--- jogo-gourmet/test/com/jogo/gourmet/MainTests.java
+++ jogo-gourmet/test/com/jogo/gourmet/MainTests.java
@@ -4,11 +4,23 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import com.jogo.gourmet.model.Massa;
+import com.jogo.gourmet.model.NaoMassa;
+
 public class MainTests {
+	Massa massa = new Massa();
+	NaoMassa naoMassa = new NaoMassa();
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void instanciaListas() {
+		assertEquals("Lasanha", massa.getPratos().get(0).getDescricao());
+		assertEquals("Bolo de Chocolate", naoMassa.getPratos().get(0).getDescricao());
 	}
 
+	@Test
+	public void comecarJogo() {
+		Tela tela = new Tela();
+		tela.comecarJogo();
+	}
+	
 }
